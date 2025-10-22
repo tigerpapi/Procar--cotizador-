86a3c0ceee5ffc917b766f5df706efd315428427
tigerpapi <tigersikis.sikis@gmail.com>
2025-10-21 19:51:29 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: { extend: {} },
   plugins: [],
-}
+};
